2e63e67d5b5766fa49aa02c88b1c0d4d2ad3e1d6
EndPx <albary6700@gmail.com>
2026-05-15 01:49:10 +0700

    feat(abis): import all 5 contract ABIs from forge build output

---
+++ src/lib/abis/index.ts
@@ -0,0 +1,5 @@
+export {AgentRegistryAbi} from "./AgentRegistry";
+export {LiquidReputationAbi} from "./LiquidReputation";
+export {RewardDistributorAbi} from "./RewardDistributor";
+export {SortinoOracleAbi} from "./SortinoOracle";
+export {SquadiumAbi} from "./Squadium";
